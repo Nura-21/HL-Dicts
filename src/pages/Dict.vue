4ba7b845dd96e1b36d2071f3febdcf30a397d7a4
Darina <dajtbaeva@gmail.com>
2023-06-07 23:55:07 +0600

    task-1 updated

--- src/pages/Dict.vue
+++ src/pages/Dict.vue
@@ -1,22 +1,34 @@
 <template>
-  <div>{{ dictName }}</div>
+  <section>
+    <v-card v-if="dictValue">
+      <v-list>
+        <v-list-subheader>Dictionary name: {{ dictName }}</v-list-subheader>
+        <v-list-item v-for="(dict, index) of dictValue" :key="index" :value="dict">
+          <v-list-item-title>{{ dict.nameRu }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-card>
+  </section>
 </template>
 
 <script>
+import { ref } from 'vue';
 export default {
   setup() {
     const store = useStore();
     const route = useRoute();
     const dictName = route.params.dict;
-    
+    const dictValue = ref([]);
+
     onMounted(async () => {
-      const dictValue = await store.getDict(dictName);
-      console.log('From Dict page: ' + dictValue);
+      dictValue.value = await store.getDict(dictName);
+      console.log('From Dict page: ' + dictValue.value);
     });
 
     return {
       store,
       dictName,
+      dictValue,
     };
   },
 };
